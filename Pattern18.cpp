2a9fa79a19a0e1009555764fe297a56fd7ef40e0
harsh-2808 <imharshmishra.28@gmail.com>
2023-08-09 21:12:09 +0530

    Number Pattern Question

---
+++ Pattern18.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+/*
+n=5
+1 1 1 1 1
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5
+*/
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int row = 1;
+    while (row <= n)
+    {
+        int col = 1;
+        while (col <= n)
+        {
+            cout << row << " ";
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
+}
