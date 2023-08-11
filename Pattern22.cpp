7ddb67199b05c1dcd86e66a1f849824ed5e75cc8
harsh-2808 <imharshmishra.28@gmail.com>
2023-08-11 13:15:46 +0530

    Number Pattern Question

---
+++ Pattern22.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+/*
+n=5
+11111
+ 2222
+  333
+   44
+    5
+*/
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int row = 1;
+    while (row <= n)
+    {
+        // Loop for printing space
+        int space = row - 1;
+        while (space)
+        {
+            cout << " ";
+            space--;
+        }
+
+        // Loop for printing stars
+        int col = 1;
+        while (col <= n - row + 1)
+        {
+            cout << row;
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
+}
